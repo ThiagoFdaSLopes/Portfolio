2e30700035803d970bbaae965392c2319c5ca31a
Thiago <programadorthiagolopes@gmail.com>
2022-12-03 09:49:12 -0300

    feat(contact): Adicionado component contact na rota

--- portfolio/src/pages/PageContact.jsx
+++ portfolio/src/pages/PageContact.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Contact from '../components/Contact';
 import HeaderHome from '../components/Header';
 
 const PageContact = () => {
   return (
     <>
     <HeaderHome />
+    <Contact />
     </>
   )
 };
